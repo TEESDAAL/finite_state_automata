5bf60daed0b4952e10d97e120700892b25a0adbe
TEESDAAL <78932086+TEESDAAL@users.noreply.github.com>
2022-08-28 20:44:43 +1200

    Added tests and now takes input

---
+++ decimal_parser/src/lib.rs
@@ -0,0 +1,109 @@
+pub enum NextChar {
+    Digit(char),
+    DecimalPoint,
+    Invalid,
+}
+
+impl NextChar {
+    pub fn from(c: char) -> Self {
+        match c {
+            '0'..='9' => NextChar::Digit(c),
+            '.' => NextChar::DecimalPoint,
+            _ => NextChar::Invalid,
+        }
+    }
+}
+
+pub enum DecimalParser {
+    Start,
+    Zero,
+    Integer,
+    DecimalPoint,
+    FloatingPoint,
+    Failed,
+}
+
+impl DecimalParser {
+    pub fn new() -> DecimalParser {
+        DecimalParser::Start
+    }
+    pub fn progress_parse(&mut self, next_char: NextChar) {
+        *self = match self {
+            Self::Start => match next_char {
+                NextChar::Digit('0') => Self::Zero,
+                NextChar::Digit(_) => Self::Integer,
+                _ => Self::Failed,
+            },
+            Self::Zero => match next_char {
+                NextChar::DecimalPoint => Self::DecimalPoint,
+                _ => Self::Failed,
+            },
+            Self::Integer => match next_char {
+                NextChar::DecimalPoint => Self::DecimalPoint,
+                NextChar::Digit(_) => Self::Integer,
+                _ => Self::Failed,
+            },
+            Self::DecimalPoint | Self::FloatingPoint => match next_char {
+                NextChar::Digit(_) => Self::FloatingPoint,
+                _ => Self::Failed,
+            },
+            Self::Failed => Self::Failed,
+        }
+    }
+    pub fn fully_parse(&mut self, input: String) {
+        for c in input.chars().collect::<Vec<char>>() {
+            self.progress_parse(NextChar::from(c));
+        }
+    }
+    pub fn valid_number(&self) -> bool {
+        match self {
+            DecimalParser::Zero | DecimalParser::Integer | DecimalParser::FloatingPoint => true,
+            _ => false,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn valid_integers() {
+        let values = vec![
+            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "31415926",
+        ];
+        for value in values {
+            let mut final_result = DecimalParser::new();
+            final_result.fully_parse(value.to_string());
+            assert_eq!(final_result.valid_number(), true)
+        }
+    }
+    #[test]
+    fn valid_floats() {
+        let values = vec!["0.1", "1.2", "3.1415926", "0.001"];
+        for value in values {
+            let mut final_result = DecimalParser::new();
+            final_result.fully_parse(value.to_string());
+            assert_eq!(final_result.valid_number(), true)
+        }
+    }
+
+    #[test]
+    fn invalid_numbers() {
+        let values = vec!["0.1.1", ".2", "7.", ".", "01"];
+        for value in values {
+            let mut final_result = DecimalParser::new();
+            final_result.fully_parse(value.to_string());
+            assert_eq!(final_result.valid_number(), false)
+        }
+    }
+
+    #[test]
+    fn invalid_misc() {
+        let values = vec!["", "abc", "3.abc", "a.3", "0.a"];
+        for value in values {
+            let mut final_result = DecimalParser::new();
+            final_result.fully_parse(value.to_string());
+            assert_eq!(final_result.valid_number(), false)
+        }
+    }
+}
--- decimal_parser/src/main.rs
+++ decimal_parser/src/main.rs
@@ -1,70 +1,20 @@
-enum NextChar {
-    Digit(char),
-    DecimalPoint,
-    Invalid,
-}
-
-impl NextChar {
-    fn from(c: char) -> Self {
-        match c {
-            '0'..='9' => NextChar::Digit(c),
-            '.' => NextChar::DecimalPoint,
-            _ => NextChar::Invalid,
-        }
-    }
-}
-
-enum DecimalParser {
-    Start,
-    Zero,
-    Integer,
-    DecimalPoint,
-    FloatingPoint,
-    Failed,
-}
-
-impl DecimalParser {
-    fn new() -> DecimalParser {
-        DecimalParser::Start
-    }
-    fn progress_parse(&mut self, next_char: NextChar) {
-        *self = match self {
-            Self::Start => match next_char {
-                NextChar::Digit('0') => Self::Zero,
-                NextChar::Digit(_) => Self::Integer,
-                _ => Self::Failed,
-            },
-            Self::Zero => match next_char {
-                NextChar::DecimalPoint => Self::DecimalPoint,
-                _ => Self::Failed,
-            },
-            Self::Integer => match next_char {
-                NextChar::DecimalPoint => Self::DecimalPoint,
-                NextChar::Digit(_) => Self::Integer,
-                _ => Self::Failed,
-            },
-            Self::DecimalPoint | Self::FloatingPoint => match next_char {
-                NextChar::Digit(_) => Self::FloatingPoint,
-                _ => Self::Failed,
-            },
-            Self::Failed => Self::Failed,
-        }
-    }
-}
+pub mod lib;
+use crate::lib::*;
+use std::io;
 fn main() {
-    let nums = vec![
-        "1", "1.0", "45.6", "3.14", "0.866", ".0", "0..", "3.1.4", "abc", "3.ad", "01", "00", "",
-    ];
-    for num in nums {
-        let mut decimal_parser = DecimalParser::new();
-        for c in num.clone().to_string().chars().collect::<Vec<char>>() {
-            decimal_parser.progress_parse(NextChar::from(c));
-        }
-        match decimal_parser {
-            DecimalParser::Zero | DecimalParser::Integer | DecimalParser::FloatingPoint => {
-                println!("✅ \"{}\" is a valid number", num)
-            }
-            _ => println!("❌\"{}\" is not a valid number", num),
+    loop {
+        println!("Please input a number to parse:");
+        let mut number = String::new();
+        io::stdin()
+            .read_line(&mut number)
+            .expect("Failed to read line");
+        let mut parser = DecimalParser::new();
+        number = (number.trim()).to_string();
+        parser.fully_parse(number.clone());
+        if parser.valid_number() {
+            println!("✅ \"{}\" is a valid number", number)
+        } else {
+            println!("❌ \"{}\" is not a valid number", number)
         }
     }
 }
