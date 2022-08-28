c40a6eca3d3918592c12067cb0a4967a8ca14734
TEESDAAL <78932086+TEESDAAL@users.noreply.github.com>
2022-08-28 19:33:45 +1200

    reworked conditions

--- decimal_parser/src/main.rs
+++ decimal_parser/src/main.rs
@@ -1,5 +1,19 @@
-use std::collections::HashSet;
-#[derive(Debug)]
+enum NextChar {
+    Digit(u8),
+    DecimalPoint,
+    Invalid,
+}
+
+impl NextChar {
+    fn from(c: char) -> Self {
+        match c {
+            '0'..='9' => NextChar::Digit(c as u8 - '0' as u8),
+            '.' => NextChar::DecimalPoint,
+            _ => NextChar::Invalid,
+        }
+    }
+}
+
 enum DecimalParser {
     Start,
     Zero,
@@ -13,63 +27,44 @@ impl DecimalParser {
     fn new() -> DecimalParser {
         DecimalParser::Start
     }
-    fn progress_parse(&mut self, next_char: char) {
+    fn progress_parse(&mut self, next_char: NextChar) {
         *self = match self {
-            Self::Start => {
-                if next_char == '0' {
-                    Self::Zero
-                } else if HashSet::from(['1', '2', '3', '4', '5', '6', '7', '8', '9'])
-                    .contains(&next_char)
-                {
-                    Self::Integer
-                } else {
-                    Self::Failed
-                }
-            }
-            Self::Zero => {
-                if next_char == '.' {
-                    Self::DecimalPoint
-                } else {
-                    Self::Failed
-                }
-            }
-            Self::Integer => {
-                if next_char == '.' {
-                    Self::DecimalPoint
-                } else if HashSet::from(['1', '2', '3', '4', '5', '6', '7', '8', '9'])
-                    .contains(&next_char)
-                {
-                    Self::Integer
-                } else {
-                    Self::Failed
-                }
-            }
-            Self::DecimalPoint | Self::FloatingPoint => {
-                if HashSet::from(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
-                    .contains(&next_char)
-                {
-                    Self::FloatingPoint
-                } else {
-                    Self::Failed
-                }
-            }
+            Self::Start => match next_char {
+                NextChar::Digit(0) => Self::Zero,
+                NextChar::Digit(_) => Self::Integer,
+                _ => Self::Failed,
+            },
+            Self::Zero => match next_char {
+                NextChar::DecimalPoint => Self::DecimalPoint,
+                _ => Self::Failed,
+            },
+            Self::Integer => match next_char {
+                NextChar::DecimalPoint => Self::DecimalPoint,
+                NextChar::Digit(_) => Self::Integer,
+                _ => Self::Failed,
+            },
+            Self::DecimalPoint | Self::FloatingPoint => match next_char {
+                NextChar::Digit(_) => Self::FloatingPoint,
+                _ => Self::Failed,
+            },
             Self::Failed => Self::Failed,
         }
     }
 }
 fn main() {
-    let nums = vec!["1", "1.0", "45.6", "3.14", ".0", "0..", "3.1.4", "abc", "3.ad", "01"];
+    let nums = vec![
+        "1", "1.0", "45.6", "3.14", "0.866", ".0", "0..", "3.1.4", "abc", "3.ad", "01", "00", "",
+    ];
     for num in nums {
         let mut decimal_parser = DecimalParser::new();
-
-        for character in num.clone().to_string().chars().collect::<Vec<char>>() {
-            decimal_parser.progress_parse(character);
+        for c in num.clone().to_string().chars().collect::<Vec<char>>() {
+            decimal_parser.progress_parse(NextChar::from(c));
         }
         match decimal_parser {
             DecimalParser::Zero | DecimalParser::Integer | DecimalParser::FloatingPoint => {
-                println!("{} is a valid number", num)
+                println!("✅ \"{}\" is a valid number", num)
             }
-            _ => println!("{} is not a valid number", num),
+            _ => println!("❌\"{}\" is not a valid number", num),
         }
     }
 }
